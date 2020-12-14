b1e941d2405864be3e65cb0141a5b2c5d39bdfd1
sabozeid <shazly.abozeid@medstreaming.com>
2020-12-14 17:38:41 +0200

    adding pproject files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlainPizza plainPizzaObj = new PlainPizza();
+            string plainPizza = plainPizzaObj.MakePizza();
+            Console.WriteLine(plainPizza);
+            PizzaDecorator chickenPizzaDecorator = new ChickenPizzaDecorator(plainPizzaObj);
+            string chickenPizza = chickenPizzaDecorator.MakePizza();
+            Console.WriteLine("\n'" + chickenPizza + "' using ChickenPizzaDecorator");
+            VegPizzaDecorator vegPizzaDecorator = new VegPizzaDecorator(plainPizzaObj);
+            string vegPizza = vegPizzaDecorator.MakePizza();
+            Console.WriteLine("\n'" + vegPizza + "' using VegPizzaDecorator");
+            Console.Read();
+        }
+
+    }
+    public interface Pizza
+    {
+        string MakePizza();
+    }
+
+    public class PlainPizza : Pizza
+    {
+        public string MakePizza()
+        {
+            return "Plain Pizza";
+        }
+    }
+
+    public abstract class PizzaDecorator : Pizza
+    {
+        protected Pizza pizza;
+        public PizzaDecorator(Pizza pizza)
+        {
+            this.pizza = pizza;
+        }
+        public virtual string MakePizza()
+        {
+            return pizza.MakePizza();
+        }
+    }
+
+    public class ChickenPizzaDecorator : PizzaDecorator
+    {
+        public ChickenPizzaDecorator(Pizza pizza) : base(pizza)
+        {
+        }
+        public override string MakePizza()
+        {
+            return pizza.MakePizza() + AddChicken();
+        }
+        private string AddChicken()
+        {
+            return ", Chicken added";
+        }
+    }
+
+    public class VegPizzaDecorator : PizzaDecorator
+    {
+        public VegPizzaDecorator(Pizza pizza) : base(pizza)
+        {
+        }
+        public override string MakePizza()
+        {
+            return pizza.MakePizza() + AddVegetables();
+        }
+        private string AddVegetables()
+        {
+            return ", Vegetables added";
+        }
+    }
+}
